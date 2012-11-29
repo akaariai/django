40bc445c8ada8891bbd32af0c26b6874dc729c45
Anssi Kääriäinen <akaariai@gmail.com>
2012-11-29 17:22:24 +0200

    Refactored save_base, part3
    Made sure .save() commits transactions only after the whole model has
    been saved. This wasn't the case in model inheritance situations.

--- tests/regressiontests/transactions_regress/models.py
+++ tests/regressiontests/transactions_regress/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Mod(models.Model):
     fld = models.IntegerField()
 
+class SubMod(Mod):
+    cnt = models.IntegerField(unique=True)
+
 class M2mA(models.Model):
     others = models.ManyToManyField('M2mB')
 
