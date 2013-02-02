2be4887498f00f55b83625e5d3ad5683548a99f6
Anssi Kääriäinen <akaariai@gmail.com>
2013-02-02 14:48:55 +0200

    Fixed #19645 -- Added tests for TransactionMiddleware

--- tests/regressiontests/middleware/models.py
+++ tests/regressiontests/middleware/models.py
@@ -1,1 +1,15 @@
-# models.py file for tests to run.
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+
+@python_2_unicode_compatible
+class Band(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+    sign_date = models.DateField()
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
